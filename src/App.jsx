44b71d9929274adaeb85951fcb687c1debe01933
Ben <61493854+bengera@users.noreply.github.com>
2025-07-10 14:14:27 +0700

    Add basic setup and styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,28 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <p>Pomo Reward</p>
-    </div>
+    <>
+      <Main>
+        <Timer></Timer>
+      </Main>
+    </>
+  );
+}
+
+function Main({ children }) {
+  return <main className="main">{children}</main>;
+}
+
+function Timer() {
+  return (
+    <>
+      <p className="timer-count">25:00</p>
+      <div className="buttons-container">
+        <button className="btn-control start">Start</button>
+        <button className="btn-control stop">Stop</button>
+        <button className="btn-control reset">Reset</button>
+      </div>
+    </>
   );
 }
 
