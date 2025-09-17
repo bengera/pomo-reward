8a11ebcc70eeaf2aa0dbf6d5e701e0d2fc64acc0
Ben <61493854+bengera@users.noreply.github.com>
2025-09-17 15:33:13 +0700

    Add intial styling changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ export function Header() {
     <div className="header">
       <div className="header__inner">
         <div className="header__content">
-          <h1>PomoReward : Version 2 🚀🚀</h1>
+          <h1 className="hero-heading">PomoReward</h1>
           {/* <img className="header__img" src="assets/tomato.svg" alt="tomato" /> */}
         </div>
       </div>
