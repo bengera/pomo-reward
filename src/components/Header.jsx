10efced10c2b8b4bfb19dfced0d4d9ec73a43479
Ben <61493854+bengera@users.noreply.github.com>
2025-10-15 16:22:33 +0700

    Add description to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,11 @@ export function Header() {
     <div className="header">
       <div className="header__inner">
         <div className="header__content">
-          <h1 className="hero-heading">PomoReward</h1>
+          <h1 className="hero-heading">Pomo-Reward</h1>
+          <p className="header__description">
+            Online Pomodoro Timer to boost your productivity. Reward yourself
+            for your focus.
+          </p>
           {/* <img className="header__img" src="assets/tomato.svg" alt="tomato" /> */}
         </div>
       </div>
